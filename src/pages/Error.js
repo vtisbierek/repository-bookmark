c4454b6b3ead4ad11d316b5e2257d2e5b23af3a3
Victor Tisbierek <metropoller@gmail.com>
2023-03-27 23:24:04 -0300

    including error 404 treatment

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,15 @@
 import React from "react";
+import {Message} from "../styles/stylesError";
+import {FaHome} from "react-icons/fa";
+import {Link} from "react-router-dom";
 
 export default function Error(){
     return (
-        <h1>Essa página não existe.</h1>
+        <Message>
+            <h1>This page does not exist.</h1>
+            <Link to="/" >
+                <FaHome color="#fff" size={50} />
+            </Link>
+        </Message>
     );
 }
---
+++ src/styles/stylesError.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Message = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+
+    h1{
+        color: #fff;
+        margin-bottom: 30px;
+    }
+`;
